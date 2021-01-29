4c303b2043b0b99d8cb1c227318cde716c368373
Víctor Vázquez <victorvazrod@ugr.es>
2021-01-29 18:10:30 +0100

    Improve run script so it closes the processes on SIGINT

--- run.sh
+++ run.sh
@@ -1,3 +1,13 @@
-taskset -c 0 python -m controls & echo $!
-taskset -c 1 python -m gui & echo $!
-taskset -c 2 python -m websockets & echo $!
+taskset -c 0 python -m controls & CONTROLS_PID=$!
+taskset -c 1 python -m gui & GUI_PID=$!
+taskset -c 2 python -m websockets & WEBSOCKETS_PID=$!
+
+trap finish SIGINT
+finish() {
+    kill -9 $CONTROLS_PID
+    kill -9 $GUI_PID
+    kill -9 $WEBSOCKETS_PID
+    exit
+}
+
+sleep infinity
